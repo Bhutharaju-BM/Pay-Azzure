1f92b4b3951c7bb512c74ae90a090ef2d5c18a8f
Bhutharaju-BM <bmbhutharaju@agile-labs.com>
2025-03-12 17:31:39 +0530

    release2

---
+++ Version 11.3/Release2/AxpertWeb/AxpertStructures/Postgre/After_import_Script.sql
@@ -0,0 +1,27 @@
+<<
+create table emp_dt 
+( empid varchar (1),
+
+empname varchar(20),
+
+doj date );
+>>
+
+<<
+insert into emp_dt (empid,empname,doj) values ('1','kjdjsdss','01/02/2023');
+>>
+<<
+insert into emp_dt (empid,empname,doj) values ('2','kjdjsdss','01/02/2023');
+>>
+<<
+insert into emp_dt (empid,empname,doj) values ('3','kjdjsdss','01/02/2023');
+>>
+<<
+insert into emp_dt (empid,empname,doj) values ('4','kjdjsdss','01/02/2023');
+>>
+<<
+insert into emp_dt (empid,empname,doj) values ('4','kjdjsdss','01/02/2023');
+>>
+<<
+insert into emp_dt (empid,empname,doj) values ('4','kjdjsdss','01/02/2023');
+>>
